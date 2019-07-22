72d1278770d9e765add9a08b318b1b5aef08ef8e
snailycy <snailycy@gmail.com>
2019-07-22 23:29:12 +0800

    fix bug

--- socialsdk/src/main/java/com/elbbbird/android/socialsdk/sso/qq/QQSSOProxy.java
+++ socialsdk/src/main/java/com/elbbbird/android/socialsdk/sso/qq/QQSSOProxy.java
@@ -5,7 +5,6 @@ import android.content.Context;
 
 import com.elbbbird.android.socialsdk.model.SocialToken;
 import com.tencent.connect.UserInfo;
-import com.tencent.connect.auth.QQToken;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
 
@@ -28,37 +27,26 @@ public class QQSSOProxy {
 
     public static void login(Context context, String appId, String scope, IUiListener listener) {
         Tencent tencent = getTencentInstance(context, appId);
-//        if (!SocialSSOProxy.isTokenValid(context)) {
+        if (!tencent.isSessionValid()) {
             tencent.login((Activity) context, scope, listener);
-//        }
+        }
     }
 
     public static void logout(Context context, String appId) {
         Tencent tencent = getTencentInstance(context, appId);
-//        if (SocialSSOProxy.isTokenValid(context)) {
-            tencent.logout(context);
-//        }
-
+        tencent.logout(context);
         QQSSOProxy.tencent = null;
     }
 
-    public static void getUserInfo(Context context, String appId, SocialToken token, IUiListener listener) {
+    public static void getUserInfo(Context context, String appId, SocialToken socialToken, IUiListener listener) {
         //这里只是为了在刷新用户信息的时候，设置一下全局变量。场景：APP升级安装，没有重新登录过程，调用该接口刷新用户信息，
         //如果没有该过程刷新Global.getContext()等常量，会出现空指针
-        getTencentInstance(context, appId);
-
-//        if (SocialSSOProxy.isTokenValid(context)) {
-            UserInfo info = new UserInfo(context, parseToken(appId, token));
+        Tencent tencent = getTencentInstance(context, appId);
+        tencent.setAccessToken(socialToken.getToken(), String.valueOf(socialToken.getExpiresTime()));
+        tencent.setOpenId(socialToken.getOpenId());
+        if (tencent != null && tencent.isSessionValid()) {
+            UserInfo info = new UserInfo(context, tencent.getQQToken());
             info.getUserInfo(listener);
-//        }
-    }
-
-    private static QQToken parseToken(String appId, SocialToken socialToken) {
-        QQToken token = new QQToken(appId);
-        //3600是随意定义的，不影响token的使用
-        token.setAccessToken(socialToken.getToken(), "3600");
-        token.setOpenId(socialToken.getOpenId());
-
-        return token;
+        }
     }
 }
